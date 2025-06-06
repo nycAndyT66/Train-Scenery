4519d9e81f7c9198494b2deee9631beb481e766f
nycAndyT66 <andyt66@nycstudents.net>
2025-06-06 11:52:57 -0400

    Update main.js
    Added Boulder on track event.

--- trainScene/main.js
+++ trainScene/main.js
@@ -1,3 +1,5 @@
+let relativeSpeed = 100;
+let randomChances = 0;
 let a = 800;
 let b = 1600;
 let c = 0;
@@ -21,6 +23,15 @@ let numDeg = 300.0;
 let numDeg2 = 120.0;
 let Radius = 40;
 let centerX, centerY;
+//rock
+let hitboxX = 340;
+let hitboxY = 210;
+let hitboxW = 130;
+let hitboxH = 130;
+let xRock = 900;
+let countRock = 0;
+let hitboxBoolean = false;
+let randomEvent = false;
 function setup() {
   createCanvas(800, 500);
   frameRate(60);
@@ -28,6 +39,9 @@ function setup() {
   centerY = height / 2;
 }
 function preload() {
+  rock = loadImage(
+    "vecteezy_an-8-bit-retro-styled-pixel-art-illustration-of-a-stone-rock_19527056.png"
+  );
   tree = loadImage(
     "pngtree-pine-tree-with-gradation-color-icon-pixel-art-png-image_6173919.png"
   );
@@ -46,9 +60,9 @@ function draw() {
   modelMoon(x2, y2);
   //models
   drawMountains(e);
-  e = e - 0.3;
+  e = e - (0.3 * relativeSpeed) / 100;
   if (e < 0 - 700) {
-    e = 1200;
+    e = 1000;
   }
   cloudModel(h + 100, 50);
   cloudModel(i + 300, 30);
@@ -68,27 +82,46 @@ function draw() {
   }
   backgroundScenery1(c);
   backgroundScenery1(d);
-  c = c - 8;
-  d = d - 8;
+  c = c - (8 * relativeSpeed) / 100;
+  d = d - (8 * relativeSpeed) / 100;
   if (c < 0 - 800 && d < 0) {
     c = 0;
     d = 800;
   }
 
   tracks(f);
-  f = f - 8;
+  f = f - (8 * relativeSpeed) / 100;
   if (f < 0.9) {
     f = 20;
   }
   trainModel();
+  tint("white");
+
+  image(rock, xRock, 270, 200, 200);
+  if (randomChances == 40) {
+    xRock -= (5 * relativeSpeed) / 100;
+    if (relativeSpeed == 0) {
+      rectMode(CORNER);
+      hitBox(hitboxX, hitboxY, hitboxH, hitboxW);
+      hitboxBoolean = true;
+    }
+  }
+  if (countRock == 40) {
+    xRock = 900;
+    hitboxBoolean = false;
+    randomEvent = false;
+    if(randomChances == 40){
+     randomChances = int(random(0, 1000)); 
+    }
+  }
   backgroundScenery2(a, 550);
   backgroundScenery2(b, 600);
   backgroundScenery2(g, 500);
   backgroundScenery2(j, 580);
-  a = a - 20;
-  b = b - 20;
-  g = g - 20;
-  j = j - 20;
+  a = a - (20 * relativeSpeed) / 100;
+  b = b - (20 * relativeSpeed) / 100;
+  g = g - (20 * relativeSpeed) / 100;
+  j = j - (20 * relativeSpeed) / 100;
   if (a < 0 - 100 && b < 0 - 100 && g < 0 - 100 && j < 0 - 100) {
     a = 800;
     b = 1400;
@@ -96,7 +129,24 @@ function draw() {
     j = 1375;
   }
   button();
+  if (randomChances != 40 && !randomEvent && relativeSpeed == 100) {
+    randomChances = int(random(0, 1000));
+  }
+  if (relativeSpeed != 100 && int(countRock) == 40) {
+    relativeSpeed += 0.5;
+    if((int)(relativeSpeed) == 100){
+      countRock = 0;
+    }
+      
 
+  } 
+  if(randomChances == 40){
+    randomEvent = true;
+    if (relativeSpeed != 0 && randomEvent) {
+      relativeSpeed -= 0.5;
+    } else {
+    }
+  }
 }
 
 function modelSun(x, y) {
@@ -113,7 +163,6 @@ function drawMountains(x) {
   triangle(100 + x, 300, 300 + x, 100, 500 + x, 300);
   triangle(300 + x, 300, 500 + x, 150, 700 + x, 300);
 }
-
 
 function backgroundScenery1(x) {
   imageMode(CENTER);
@@ -169,39 +218,43 @@ function tracks(x) {
   rect(0, 318, 800, 6);
 }
 function trainModel() {
-  if(!dayTime && count > 25){
-    let c2 = color(255,255,0)
-    c2.setAlpha(100)
-    fill(c2)
-    triangle(480,280,650,260,650,300)
+  if (!dayTime && count > 25) {
+    let c2 = color(255, 255, 0);
+    c2.setAlpha(100);
+    fill(c2);
+    triangle(480 - 200, 280, 650 - 200, 260, 650 - 200, 300);
   }
+
   fill("silver");
   stroke(1);
   ellipseMode(CENTER);
-  ellipse(479, 280, 10, 50);
-  fill('silver')
-  rectMode(CENTER)
-  rect(359,298,170,30)
-  fill("black")
-  rect(380, 280, 200, 50);
-  rect(373, 280, 200, 40);
+  ellipse(479 - 200, 280, 10, 50);
+  fill("silver");
+  rectMode(CENTER);
+  rect(359 - 200, 298, 170, 30);
+  fill("black");
+  rect(380 - 200, 280, 200, 50);
+  rect(373 - 200, 280, 200, 40);
   fill("grey");
-  rect(455, 307, 50, 15);
-  rect(475, 297, 70, 10);
-  rect(455, 296, 50, 20);
-  rect(300, 255, 50, 60);
-  rect(295, 225, 75, 15);
-  triangle(257, 217, 333, 217, 295, 210);
-  triangle(440,210,480,210,460,240)
-  rect(460,240,20,30)
+  rect(455 - 200, 307, 50, 15);
+  rect(475 - 200, 297, 70, 10);
+  rect(455 - 200, 296, 50, 20);
+  rect(300 - 200, 255, 50, 60);
+  rect(295 - 200, 225, 75, 15);
+  triangle(257 - 200, 217, 333 - 200, 217, 295 - 200, 210);
+  triangle(440 - 200, 210, 480 - 200, 210, 460 - 200, 240);
+  rect(460 - 200, 240, 20, 30);
+  ellipse(410 - 200, 235, 20, 20);
+  rect(410 - 200, 245, 20, 20);
+  ellipse(360 - 200, 235, 20, 20);
+  rect(360 - 200, 245, 20, 20);
   ellipseMode(CENTER);
   stroke(2);
   fill("maroon");
-  ellipse(300, 305, 40, 40);
-  ellipse(350, 305, 40, 40);
-  ellipse(470, 317, 15, 15);
-  ellipse(445, 317, 15, 15);
-  rectMode(CORNER);
+  ellipse(300 - 200, 305, 40, 40);
+  ellipse(350 - 200, 305, 40, 40);
+  ellipse(470 - 200, 317, 15, 15);
+  ellipse(445 - 200, 317, 15, 15);
 }
 function cloudModel(x, y) {
   noStroke();
@@ -217,35 +270,35 @@ function cloudModel(x, y) {
 }
 function time() {
   if (!dayTime) {
-    if (count < 10*2) {
+    if (count < 10 * 2) {
       numDeg += 4.5;
       numDeg2 += 4.5;
-      cr += 12/2;
-      cg += 0.5/2;
-      cb -= 5.5/2;
+      cr += 12 / 2;
+      cg += 0.5 / 2;
+      cb -= 5.5 / 2;
       count += 1;
-    } else if (count < 20*2) {
+    } else if (count < 20 * 2) {
       numDeg += 4.5;
       numDeg2 += 4.5;
-      cr -= 22.5/2;
-      cg -= 16.5/2;
-      cb -= 9.5/2;
+      cr -= 22.5 / 2;
+      cg -= 16.5 / 2;
+      cb -= 9.5 / 2;
       count += 1;
     }
   }
 
   if (dayTime) {
-    if (count > 10*2) {
-      cr += 22.5/2; // Reverse decrease
-      cg += 16.5/2;
-      cb += 9.5/2;
-      count -= 1; 
+    if (count > 10 * 2) {
+      cr += 22.5 / 2; // Reverse decrease
+      cg += 16.5 / 2;
+      cb += 9.5 / 2;
+      count -= 1;
       numDeg += 4.5;
       numDeg2 += 4.5;
     } else if (count > 0) {
-      cr -= 12/2; // Reverse increase
-      cg -= 0.5/2;
-      cb += 5.5/2;
+      cr -= 12 / 2; // Reverse increase
+      cg -= 0.5 / 2;
+      cb += 5.5 / 2;
       numDeg += 4.5;
       numDeg2 += 4.5;
       if (count == 0) {
@@ -255,18 +308,42 @@ function time() {
     }
   }
 }
+function hitBox(x, y, w, h) {
+  noStroke();
+  fill(color(255, 255, 255, 0));
+  rect(x, y, w, h);
+  stroke(1);
+  strokeWeight(1);
+  
+}
 function mousePressed() {
-  if (mouseX > lx && mouseY > ly && mouseX < lx + lh && mouseY < ly + lw) {
+  if (
+    mouseX > lx &&
+    mouseY > ly &&
+    mouseX < lx + lh &&
+    mouseY < ly + lw &&
+    (count == 0 || count == 40)
+  ) {
     dayTime = !dayTime;
+  }
+  if (
+    mouseX > hitboxX &&
+    mouseY > hitboxY &&
+    mouseX < hitboxX + hitboxH &&
+    mouseY < hitboxY + hitboxW &&
+    countRock != 40 &&
+    hitboxBoolean == true
+  ) {
+    countRock++;
   }
 }
 function button() {
   rectMode(CORNER);
   fill("white");
-  rect(750, 0, 50,70);
+  rect(750, 0, 50, 70);
   rectMode(CORNER);
   fill("red");
   rect(lx, ly, lh, lw);
-  fill('black')
-  text('Time',762,10)
+  fill("black");
+  text("Time", 762, 10);
 }
